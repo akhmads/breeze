c5d3d1e3ea49fd8625f092530ce7d8de2a30a94d
Akhmad <akhmadshaleh@gmail.com>
2023-10-05 21:44:55 +0700

    add post crud

--- resources/views/livewire/post/post-table.blade.php
+++ resources/views/livewire/post/post-table.blade.php
@@ -26,6 +26,7 @@
                     </a>
                 </div>
             </div>
+
             <div class="w-full mx-auto">
                 <div class="bg-white border border-gray-200 rounded-xl shadow-sm overflow-hidden dark:bg-slate-900 dark:border-gray-700">
                     <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
@@ -33,7 +34,7 @@
                         <tr>
                             <th class="px-4 py-2 text-left">Title</th>
                             <th class="px-4 py-2 text-left w-[200px]">Created At</th>
-                            <th class="px-4 py-2 text-left"></th>
+                            <th class="px-4 py-2 text-left">Action</th>
                         </tr>
                         </thead>
                         <tbody class="divide-y divide-gray-200 dark:divide-gray-700">
